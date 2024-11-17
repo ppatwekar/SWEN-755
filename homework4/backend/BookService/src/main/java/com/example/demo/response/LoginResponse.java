5d6c6dcc6bc6fbf05df46cf922543ce322cea045
Prathamesh Patwekar <pp7138@rit.edu>
2024-11-17 17:42:39 -0500

    working frontend to backend

--- homework4/backend/BookService/src/main/java/com/example/demo/response/LoginResponse.java
+++ homework4/backend/BookService/src/main/java/com/example/demo/response/LoginResponse.java
@@ -1,6 +1,14 @@
 package com.example.demo.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class LoginResponse {
+
+    public LoginResponse() {
+    }
+
+    @Getter @Setter
     private String sessionId;
 
     public LoginResponse(String sessionId) {
